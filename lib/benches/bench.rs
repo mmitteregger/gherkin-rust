f37fb06b3800158025027473c40bbb83e2100e67
Michael Mitteregger <michael.mitteregger@gmx.at>
2020-01-21 22:26:33 +0100

    Upgrade to Gherkin 9 - fix benchmarks

--- lib/benches/bench.rs
+++ lib/benches/bench.rs
@@ -8,13 +8,16 @@ mod tests {
     use std::path::{Path, PathBuf};
     use test::Bencher;
 
-    use gherkin::event;
-    use gherkin::Parser;
+    use cucumber_messages::id_generator::IncrementingIdGenerator;
+
+    use gherkin::{self, GherkinDocumentBuilder, IncludeOptions, Parser};
 
     #[bench]
     fn bench_parser_good_features(bencher: &mut Bencher) {
         let features = read_features("good");
-        let mut parser = Parser::default();
+        let mut id_generator = IncrementingIdGenerator::new();
+        let builder = GherkinDocumentBuilder::with_id_generator(&mut id_generator);
+        let mut parser = Parser::with_builder(builder);
 
         bencher.iter(|| {
             for feature in &features {
@@ -26,7 +29,9 @@ mod tests {
     #[bench]
     fn bench_parser_bad_features(bencher: &mut Bencher) {
         let features = read_features("bad");
-        let mut parser = Parser::default();
+        let mut id_generator = IncrementingIdGenerator::new();
+        let builder = GherkinDocumentBuilder::with_id_generator(&mut id_generator);
+        let mut parser = Parser::with_builder(builder);
 
         bencher.iter(|| {
             for feature in &features {
@@ -37,35 +42,53 @@ mod tests {
 
     #[bench]
     fn bench_compiler_good_features(bencher: &mut Bencher) {
-        let features = read_features("good");
+        let feature_paths = feature_paths_iter("good").collect::<Vec<PathBuf>>();
 
         bencher.iter(|| {
-            for feature in &features {
-                let data = feature.clone();
-                event::generate(data, "bench_compiler_good_features").unwrap();
-            }
+            let mut id_generator = IncrementingIdGenerator::new();
+            gherkin::parse_paths(
+                &feature_paths,
+                IncludeOptions {
+                    source: false,
+                    gherkin_document: false,
+                    pickles: true,
+                },
+                &mut id_generator,
+            ).unwrap();
         });
     }
 
     #[bench]
     fn bench_compiler_bad_features(bencher: &mut Bencher) {
-        let features = read_features("bad");
+        let feature_paths = feature_paths_iter("bad").collect::<Vec<PathBuf>>();
 
         bencher.iter(|| {
-            for feature in &features {
-                let data = feature.clone();
-                event::generate(data, "bench_compiler_bad_features").unwrap_err();
-            }
+            let mut id_generator = IncrementingIdGenerator::new();
+            gherkin::parse_paths(
+                &feature_paths,
+                IncludeOptions {
+                    source: false,
+                    gherkin_document: false,
+                    pickles: true,
+                },
+                &mut id_generator,
+            ).unwrap();
         });
     }
 
     fn read_features<P: AsRef<Path>>(path: P) -> Vec<String> {
-        fs::read_dir(PathBuf::from("testdata").join(path))
+        feature_paths_iter(path)
+            .map(fs::read_to_string)
+            .map(Result::unwrap)
+            .collect()
+    }
+
+    fn feature_paths_iter<P: AsRef<Path>>(path: P) -> impl Iterator<Item=PathBuf> {
+        fs::read_dir(PathBuf::from("../testdata").join(path))
             .unwrap()
             .into_iter()
             .map(Result::unwrap)
             .filter(|entry| entry.file_name().to_string_lossy().ends_with(".feature"))
-            .map(|entry| fs::read_to_string(entry.path()).unwrap())
-            .collect::<Vec<String>>()
+            .map(|entry| entry.path())
     }
 }
